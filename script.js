14fc027023436c50a1f876e6b57129b58c6f45f4
User1 <User1@gmail.com>
2025-02-05 19:23:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function beforesubmit(){
+    let inputdate = document.querySelector(".inputdate")
+    let outputdate = document.querySelector(".outputdate")
+    console.log(inputdate.value)
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN")
+    outputdate.value = formattedDate
+}
