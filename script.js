4d71e1ad9dd5e149ba830b488204e902236b87ff
User1 <User1@gmail.com>
2025-02-05 23:28:23 +0530

    Captcha Added

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function beforesubmit(){
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN")
     outputdate.value = formattedDate
 }
+
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+     } }
+     
+setInterval(timestamp, 500); 
